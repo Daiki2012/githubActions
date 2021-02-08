88e59b137c2c966ae02420cbd3ee8d633acc1928
Daiki <haraguti2002@msn.com>
2021-02-08 19:15:33 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         8 変更したけど、その前のファイルはどうなるの. cleanup
+         9 変更したけど、その前のファイルはどうなるの. cleanup
         </p>
         <a
           className="App-link"
